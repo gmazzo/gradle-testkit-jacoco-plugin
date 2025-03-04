836df3e93e3c5346a1c39766582ab8e1edf0f2cd
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-04 21:09:04 +0100

    Fixed broken classpath

--- plugin/src/test/resources/project/src/main/java/org/test/myplugin/MyPlugin.java
+++ plugin/src/test/resources/project/src/main/java/org/test/myplugin/MyPlugin.java
@@ -1,11 +1,16 @@
 package org.test.myplugin;
 
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 import org.test.myplugin.utils.Utils;
+import com.squareup.javapoet.ClassName;
 
-class MyPlugin {
+class MyPlugin implements Plugin<Project> {
 
-    void doApply() {
+    @Override
+    public void apply(Project target) {
         Utils.INSTANCE.doSomething();
+        ClassName.bestGuess("java.lang.String");
     }
 
 }
